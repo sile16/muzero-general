c3be91d29c4055551e2eae13b57d15d51577ac26
Matt Robertson <sile16@gmail.com>
2025-01-26 23:18:05 -0600

    human input improved

--- games/backgammon.py
+++ games/backgammon.py
@@ -44,7 +44,7 @@ class MuZeroConfig:
         self.num_workers = 8  # Number of simultaneous threads/workers self-playing to feed the replay buffer
         self.selfplay_on_gpu = False
         self.max_moves = 4000  # Maximum number of moves if game is not finished before
-        self.num_simulations = 20  # Number of future moves self-simulated
+        self.num_simulations = 30  # Number of future moves self-simulated
         self.discount = 0.997  # Chronological discount of the reward
         self.temperature_threshold = None  # Number of moves before dropping the temperature given by visit_softmax_temperature_fn to 0 (ie selecting the best action). If None, visit_softmax_temperature_fn is used every time
 
